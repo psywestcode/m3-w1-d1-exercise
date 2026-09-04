1963c848abc22f36d281b43647ee9e4b89f6d650
psylesh <70608874+psylesh@users.noreply.github.com>
2026-09-04 15:53:48 -0700

    task 3

--- node-http/server.js
+++ node-http/server.js
@@ -1,14 +1,51 @@
 // server.js
 var http = require('http');
+var path = require('path');
+var fs = require('fs');
 
 var hostname = 'localhost';
 var port = 3000;
 
-var server = http.createServer((req, res) => {
-    console.log(req.headers); // Log the request headers in terminal
-    res.statusCode = 200; // Success code
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello World!</h1></body></html>'); // Serve HTML string
+var server = http.createServer(function(req, res){
+    console.log(`Request for ${req.url} by method ${req.method}`);
+
+    if (req.method === 'GET') {
+        let fileUrl = req.url;
+        // Match root index
+        if (fileUrl === '/') {
+            fileUrl = '/index.html';
+        }
+
+        // Resolve to absolute path on the system
+        const filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+
+        if (fileExt === '.html') {
+            // Check if the file is accessible
+            fs.access(filePath, function (err){
+                if (err) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+                    return;
+                }
+                // If accessible, stream the file
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            // extension is not .html
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
+        }
+    } else {
+        // Request method is not GET
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
+    }
 });
 
 server.listen(port, hostname, () => {
